a3948c076454d2833c9492091c00d599c7d8db21
Vinicius Lidington <vinicius.lins@encora.com>
2026-02-05 14:21:57 -0300

    Add todo component

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './components/model';
 
 
-function App(){
-  return <div className='App'>Hello World</div>;
+const App:React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (event: React.SubmitEvent) => {
+    event.preventDefault();
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone: false }])
+      setTodo("")
+      console.log(todos)
+    }
+  };
+
+
+  return <div className='App'>
+    <span className="heading">Taskify</span>
+    <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+
+    {todos.map((target)=>(
+      <li>{target.todo}</li>
+    ))}
+    
+  </div>;
+
 }
 
 export default App;
---
+++ typescript-tutorial/src/components/InputField.tsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react'
+import './styles.css'
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (event: React.SubmitEvent) => void;
+}
+
+const InputField = ({todo, setTodo, handleAdd}:Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+
+  return (
+    <form 
+				className='input' 
+				onSubmit= {(event) => {
+					handleAdd(event)
+					inputRef.current?.blur();
+				}}
+				>
+        <input 
+						ref={inputRef}
+						type="input"
+            value={todo}
+            onChange={ (event) => setTodo(event.target.value)
+            }
+            placeholder='Enter a task' className='input__box'></input>
+
+        <button className='input_submit' type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
